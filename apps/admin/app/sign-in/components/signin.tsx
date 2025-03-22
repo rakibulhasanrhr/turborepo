40616dfb55767fb1f798cd6b1ee1e857781cb753
rakibulhasanrhr <rhrony7475@gmail.com>
2025-03-22 12:26:12 +0600

    sign in form

---
+++ apps/admin/app/sign-in/components/signin.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function Signin() {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Form submitted");
+    };
+    return (
+        <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+            <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
+                Welcome to Aceternity
+            </h2>
+            <form className="my-8" onSubmit={handleSubmit}>
+                <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+                    <LabelInputContainer>
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" placeholder="Enter Your Email" type="email" />
+                    </LabelInputContainer>
+                </div>
+                <LabelInputContainer className="mb-4">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" placeholder="••••••••" type="password" />
+                </LabelInputContainer>
+
+                <Button
+                    className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+                    type="submit"
+                >
+                    Sign In &rarr;
+                </Button>
+                <Button
+                    className="group/btn relative block h-10 w-full mt-2 rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+                    type="submit"
+                >
+                    Sign in with Google &rarr;
+                </Button>
+
+
+
+            </form>
+        </div>
+    );
+}
+
+
+const LabelInputContainer = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return (
+        <div className={cn("flex w-full flex-col space-y-2", className)}>
+            {children}
+        </div>
+    );
+};
